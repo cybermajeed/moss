6f02ad7859ba9d2fca3751dec824d1389a186b24
cybermajeed <abdul.zxmajeed@gmail.com>
2026-06-08 23:39:42 +0530

    ok thats how it works...

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Header: React.FC = () => (
-  <div className="bg-red-500 flex items-center gap-2 border-b border-moss-900 pb-3">
+  <div className="bg-moss-100 flex items-center gap-2 border-b border-moss-900 pb-3">
     <div className="w-6 h-6 rounded bg-transparent flex items-center justify-center">
       <img className=" rounded-lg" src="/icons/icon48.png" alt="logo" />
     </div>
     <div>
-      <h1 className="text-moss-50 text-sm font-bold tracking-widest">MOSS</h1>
-      <p className="text-moss-500 text-[10px] tracking-wide">Modular Override Script System</p>
+      <h1 className="text-moss-900 text-sm font-bold tracking-widest">MOSS</h1>
+      <p className="text-moss-600 text-[10px] tracking-wide">Modular Override Script System</p>
     </div>
   </div>
 );
