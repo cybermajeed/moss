3dccd0966b9715b051c7090a86ac6eb12bd4d277
cybermajeed <abdul.zxmajeed@gmail.com>
2026-06-09 07:29:26 +0530

    duh

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,24 +1,10 @@
+import Toolbar from "@/shared/components/Toolbar";
 import React from "react";
-import Header from "@/shared/components/Header";
-import Footer from "@/shared/components/Footer";
-import { Home } from "lucide-react";
 
 const App: React.FC = () => {
   return (
-    <div className="flex justify-between flex-col h-dvh bg-moss-950 p-4 gap-4">
-      <Header />
-      <div className="flex items-center justify-between border border-moss-900 rounded px-3 py-2">
-        <span className="text-moss-500 text-xs tracking-widest uppercase">System</span>
-        <div>
-          <Home size={30} className="text-moss-200" />
-        </div>
-      </div>
-
-      <div className="flex-1 border border-moss-900 rounded p-3">
-        <p className="text-moss-500 text-xs tracking-widest uppercase mb-3">Modules</p>
-        <p className="text-moss-100/40 text-xs">No modules loaded yet.</p>
-      </div>
-      <Footer />
+    <div className="wrapper flex justify-between flex-col h-dvh bg-moss-950 p-4 gap-4">
+      <Toolbar />
     </div>
   );
 };
---
+++ src/shared/components/Toolbar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Toolbar: React.FC = () => (
+    <div>d</div>
+);
+
+export default Toolbar
+
