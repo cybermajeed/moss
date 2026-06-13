56cdaa10865c29e77e49d69433ebef517da3f82d
cybermajeed <abdul.zxmajeed@gmail.com>
2026-06-13 19:42:01 +0530

    fixed theme issues

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Toolbar from "@/popup/components/Toolbar";
 import Container from "@/popup/components/Container";
 import { ThemeContext } from "@/shared/context/ThemeContext";
 const App = () => {
-  const [dark, setDark] = useState(false);
-
+  const [dark, setDark] = useState(true);
+  useEffect(() => {
+    document.documentElement.classList.toggle(
+      "dark",
+      dark
+    );
+  }, [dark]);
   return (
     <ThemeContext.Provider value={{ dark, setDark }}>
-
-      <div className={`${dark ? "dark" : ""} flex flex-col w-full h-dvh p-3 gap-3 bg-moss-900 dark:bg-moss-950`}>
+      <div className={`flex flex-col w-full h-dvh p-3 gap-3 bg-moss-300 dark:bg-moss-900`}>
         <Toolbar />
         <Container />
       </div>
